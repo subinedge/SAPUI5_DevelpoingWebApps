a4f8bc0c0e1430348455a5df4f5e4c9c6f998b49
Subin S <subinsamrat96@gmail.com>
2020-07-19 16:23:13 +0000

    disabled batch call for debugging

--- webapp/Component.js
+++ webapp/Component.js
@@ -19,6 +19,8 @@ sap.ui.define([
 		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
+			
+			this.getModel().setUseBatch(false);
 
 			// enable routing
 			// this.getRouter().initialize();
