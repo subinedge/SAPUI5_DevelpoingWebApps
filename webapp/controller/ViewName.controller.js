c715cc6f2de413e8fadab45fce0d34ec36f404e4
Subin S <subinsamrat96@gmail.com>
2020-08-03 16:13:25 +0000

    added fragments to view

--- webapp/controller/ViewName.controller.js
+++ webapp/controller/ViewName.controller.js
@@ -5,43 +5,64 @@
 // module load once finished (this is called asynchronously)....function execution starts..
 
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"projectNamespace/projectName/model/formatter",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], 
+		"sap/ui/core/mvc/Controller",
+		"sap/m/MessageToast",
+		"projectNamespace/projectName/model/formatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator"
+	],
 
-function (Controller, MessageToast, formatter, Filter, FilterOperator) {
-	
-	return Controller.extend("projectNamespace.projectName.controller.ViewName", {
-		
-		formatter: formatter,
-		
-		onShowMessage: function () {
-			MessageToast.show("hey guys, I am Suga !");
-		},
-		
-		onFilterProducts: function(oEvent) {
-			// build filter array
-			var aFilter = [];
-			var sQuery = oEvent.getParameter("query");
-			aFilter.push(new Filter("SupplierID", FilterOperator.Contains, sQuery));
-			
-			// filter binding
-			var oList = this.getView().byId("invoiceList");
-			var oBinding = oList.getBinding("items");
-			oBinding.filter(aFilter);
-		},
-		
-		onItemSelected: function(oEvent) {
-			var oSelectedItem = oEvent.getParameter('listItem');
-			var oContext = oSelectedItem.getBindingContext();
-			var oPath = oContext.getPath();
-			
-			var oPanel = this.getView().byId('productHeaderDetails');
-			oPanel.bindElement({path: oPath});
-			oPanel.setVisible(true);
-		}
+	function (Controller, MessageToast, formatter, Filter, FilterOperator) {
+
+		return Controller.extend("projectNamespace.projectName.controller.ViewName", {
+
+			onInit: function () {
+				if (!this.oDialog) {
+					this.oDialog = sap.ui.xmlfragment("projectNamespace.projectName.fragments.userInfo", this);
+				}
+			},
+
+			formatter: formatter,
+
+			onShowMessage: function () {
+				MessageToast.show("hey guys, I am Suga !");
+			},
+
+			showDialog: function () {
+				var viewInp1 = this.getView().byId('inp1').getValue();
+				var viewInp2 = this.getView().byId('inp2').getValue();
+
+				sap.ui.getCore().byId('inpFragFirst').setValue(viewInp1);
+				sap.ui.getCore().byId('inpFragLast').setValue(viewInp2);
+				this.oDialog.open();
+			},
+
+			handleClose: function () {
+				this.oDialog.close();
+			},
+
+			onFilterProducts: function (oEvent) {
+				// build filter array
+				var aFilter = [];
+				var sQuery = oEvent.getParameter("query");
+				aFilter.push(new Filter("SupplierID", FilterOperator.Contains, sQuery));
+
+				// filter binding
+				var oList = this.getView().byId("invoiceList");
+				var oBinding = oList.getBinding("items");
+				oBinding.filter(aFilter);
+			},
+
+			onItemSelected: function (oEvent) {
+				var oSelectedItem = oEvent.getParameter('listItem');
+				var oContext = oSelectedItem.getBindingContext();
+				var oPath = oContext.getPath();
+
+				var oPanel = this.getView().byId('productHeaderDetails');
+				oPanel.bindElement({
+					path: oPath
+				});
+				oPanel.setVisible(true);
+			}
+		});
 	});
-});
